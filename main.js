82149c75531febe13930db19c02504ba402228e7
AkshayaAkshaj <87810890+AkshayaAkshaj@users.noreply.github.com>
2022-01-07 17:05:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload(){
+
+}
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);
+
+canvas=createCanvas(550,550);
+canvas.position(560,100);
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses)
+}
+function draw(){
+background('#ff99c2');
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
